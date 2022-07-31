16ac8256436ffd5752db690aeb73b5f840b27893
Rachel <rachel.aumaugher@gmail.com>
2022-07-31 01:29:21 -0400

    Add project files.

---
+++ AbridgedFoodNutrient.cs
@@ -0,0 +1,12 @@
+﻿namespace USDAApiClient
+{
+    public class AbridgedFoodNutrient
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public float Amount { get; set; }
+        public string UnitName { get; set; }
+        public string DerivationCode { get; set; }
+        public string DerivationDescription { get; set; }
+    }
+}
---
+++ SearchResultFood.cs
@@ -0,0 +1,19 @@
+﻿namespace USDAApiClient
+{
+    public class SearchResultFood
+    {
+        public int FdcID { get; set; }
+        public string DataType { get; set; }
+        public string Description { get; set; }
+        public string FoodCode { get; set; }
+        public AbridgedFoodNutrient[] FoodNutrients { get; set; }
+        public string PublicationDate { get; set; }
+        public string ScientificName { get; set; }
+        public string BrandOwner { get; set; }
+        public string GtinUpc { get; set; }
+        public string Ingredients { get; set; }
+        public string AdditionalDescriptions { get; set; }
+        public string AllHighlightFields { get; set; }
+        public float Score { get; set; }
+    }
+}
