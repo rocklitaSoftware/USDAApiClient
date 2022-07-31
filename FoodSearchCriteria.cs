c0f38d7574a6e87c720079797e13386789f81961
Rachel <rachel.aumaugher@gmail.com>
2022-07-31 01:51:38 -0400

    Completed AbridgedFoodNutrient, FoodSearchCriteria, SearchResult, SearchResultFood

---
+++ FoodSearchCriteria.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace USDAApiClient
+{
+    public class FoodSearchCriteria
+    {
+        public string Description { get; set; }
+        public string Query { get; set; }
+        public DataType[] DataTypes { get; set; }
+        public int PageSize { get; set; }
+        public SortBy SortBy { get; set; }
+        public SortOrder SortOrder { get; set; }
+        public string BrandOwner { get; set; }
+        public TradeChannel[] TradeChannels { get; set; }
+        public string StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+
+    public enum DataType
+    {
+        Branded,
+        Foundation,
+        Survey,
+        SR_Legacy
+    }
+
+    public enum SortBy
+    {
+        dataType_keyword,
+        lowercaseDescription_keyword,
+        fdcId,
+        publishedDate
+    }
+
+    public enum SortOrder
+    {
+        asc,
+        desc
+    }
+
+    public enum TradeChannel
+    {
+        CHILD_NUTRITION_FOOD_PROGRAMS, DRUG, FOOD_SERVICE, GROCERY, MASS_MERCHANDISING, MILITARY, ONLINE, VENDING
+    }
+}
---
+++ SearchResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USDAApiClient
+{
+    public class SearchResult
+    {
+        public FoodSearchCriteria FoodSearchCriteria { get; set; }
+        public int TotalHits { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public SearchResultFood[] SearchResultFoods { get; set; }
+    }
+}
